1ac37b53b429a1dd6479ec3147b906f36eb87cbf
einshart0000000 <kimsabin0416@gmail.com>
2018-03-13 17:53:08 +0900

    dd

--- js/nanum.js
+++ js/nanum.js
@@ -1,3 +1,4 @@
+//스와이퍼
 $(function(){
   var swiper2 = new Swiper('.swiper2',{
     navigation:{
@@ -6,3 +7,11 @@ $(function(){
     }
   });
 })
+
+//공지사항 롤링
+notice=setInterval(noticeRolling,4000);
+function noticeRolling(){
+  $('.notice li').first().slideUp(function(){
+    $(this).appendTo($('.notice ul')).slideDown();
+  })
+}
